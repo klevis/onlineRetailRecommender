642b463543feddfb8a5ec05de70f8b1f6201efe4
klevis.ramo <klevisgramo@gmail.com>
2017-11-14 21:53:35 +0100

    init ui

--- src/main/java/ramo/klevis/Run.java
+++ src/main/java/ramo/klevis/Run.java
@@ -1,7 +1,41 @@
 package ramo.klevis;
 
+import ramo.klevis.ui.UI;
+
+import javax.swing.*;
+import java.io.File;
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by klevis.ramo on 11/14/2017.
  */
 public class Run {
+    public static void main(String[] args) throws Exception {
+        setHadoopHomeEnvironmentVariable();
+        SwingUtilities.invokeAndWait(() -> {
+            try {
+                new UI();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    private static void setHadoopHomeEnvironmentVariable() throws Exception {
+        HashMap<String, String> hadoopEnvSetUp = new HashMap<>();
+        hadoopEnvSetUp.put("HADOOP_HOME", new File("winutils-master/hadoop-2.8.1").getAbsolutePath());
+        Class<?> processEnvironmentClass = Class.forName("java.lang.ProcessEnvironment");
+        Field theEnvironmentField = processEnvironmentClass.getDeclaredField("theEnvironment");
+        theEnvironmentField.setAccessible(true);
+        Map<String, String> env = (Map<String, String>) theEnvironmentField.get(null);
+        env.clear();
+        env.putAll(hadoopEnvSetUp);
+        Field theCaseInsensitiveEnvironmentField = processEnvironmentClass.getDeclaredField("theCaseInsensitiveEnvironment");
+        theCaseInsensitiveEnvironmentField.setAccessible(true);
+        Map<String, String> cienv = (Map<String, String>) theCaseInsensitiveEnvironmentField.get(null);
+        cienv.clear();
+        cienv.putAll(hadoopEnvSetUp);
+    }
 }
---
+++ src/main/java/ramo/klevis/ui/UI.java
@@ -0,0 +1,81 @@
+package ramo.klevis.ui;
+
+import javax.swing.*;
+import javax.swing.plaf.FontUIResource;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/**
+ * Created by klevis.ramo on 10/29/2017.
+ */
+public class UI {
+    private static final int FRAME_WIDTH = 1200;
+    private static final int FRAME_HEIGHT = 628;
+
+    private JFrame mainFrame;
+    private JPanel mainPanel;
+    private JProgressBar progressBar;
+
+    public UI() throws Exception {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        UIManager.put("Table.font", new FontUIResource(new Font("Dialog", Font.ITALIC, 14)));
+        UIManager.put("ProgressBar.font", new FontUIResource(new Font("Dialog", Font.BOLD, 16)));
+        initUI();
+    }
+
+    private void initUI() throws Exception {
+        mainFrame = createMainFrame();
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+
+        addSignature();
+
+        mainFrame.add(mainPanel);
+        mainFrame.setVisible(true);
+    }
+
+    private JFrame createMainFrame() {
+        JFrame mainFrame = new JFrame();
+        mainFrame.setTitle("Book Recommender");
+        mainFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        mainFrame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+        mainFrame.setLocationRelativeTo(null);
+        mainFrame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+        ImageIcon imageIcon = new ImageIcon("icon.png");
+        mainFrame.setIconImage(imageIcon.getImage());
+
+        return mainFrame;
+    }
+
+
+    private void showProgressBar() {
+        SwingUtilities.invokeLater(() -> {
+            progressBar = createProgressBar(mainFrame);
+            progressBar.setString("Training Algorithm!Please wait it may take one or two minutes");
+            progressBar.setStringPainted(true);
+            progressBar.setIndeterminate(true);
+            progressBar.setVisible(true);
+            mainFrame.repaint();
+        });
+    }
+
+    private JProgressBar createProgressBar(JFrame mainFrame) {
+        JProgressBar jProgressBar = new JProgressBar(JProgressBar.HORIZONTAL);
+        jProgressBar.setVisible(false);
+        mainFrame.add(jProgressBar, BorderLayout.NORTH);
+        return jProgressBar;
+    }
+
+    private void addSignature() {
+        JLabel signature = new JLabel("ramok.tech", JLabel.HORIZONTAL);
+        signature.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 20));
+        signature.setForeground(Color.BLUE);
+        mainPanel.add(signature, BorderLayout.SOUTH);
+    }
+}
